e3044fe5afc71c122d4322cba2ca0bd3cf8350f9
Sean Hagstrom <sean@seanhagstrom.com>
2025-06-29 11:58:10 +0100

    chore: refactor view components with generic constraints

--- src/client/views.tsx
+++ src/client/views.tsx
@@ -1,7 +1,7 @@
 import { memo as memoRender } from "react"
+import { Flex, Button } from "@radix-ui/themes"
 
 import type { InteractionHandler } from "./framework"
-import { Flex, Button, Text } from "@radix-ui/themes"
 
 export type ViewEvent<El = Element, Ev = Event> = React.UIEvent<El, Ev>
 
@@ -12,9 +12,12 @@ export function ComponentLayout(props: React.PropsWithChildren & { className?: s
 }
 
 export function ComponentButton<
-    ButtonInteraction extends React.MouseEvent<HTMLButtonElement & HTMLAnchorElement>,
-    ClickHandler extends InteractionHandler<ButtonInteraction>>
-    (props: { onClick: ClickHandler } & React.PropsWithChildren) {
+    ViewElement extends HTMLButtonElement,
+    ButtonInteraction extends React.MouseEvent<ViewElement>,
+    ClickHandler extends InteractionHandler<ButtonInteraction>
+>(
+    props: { onClick: ClickHandler } & React.PropsWithChildren
+) {
     return <>
         <Button className="btn" onClick={props.onClick}>
             {props.children}
