fd88cc14def31d6f75a4f757db2b905bb4b3600d
Sean Hagstrom <sean@seanhagstrom.com>
2025-12-08 18:33:20 -0800

    chore: add civet as dev dependency

--- modules/app/vite.config.js
+++ modules/app/vite.config.js
@@ -2,11 +2,15 @@ import { defineConfig } from "vite"
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import tsconfigPaths from "vite-tsconfig-paths"
+import civetVitePlugin from '@danielx/civet/vite'
 
 export default defineConfig({
     plugins: [
         react(),
         tailwindcss(),
         tsconfigPaths(),
+        civetVitePlugin({
+            ts: 'civet',
+        }),
     ],
 })
